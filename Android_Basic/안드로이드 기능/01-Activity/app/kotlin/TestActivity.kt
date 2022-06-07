5d9dd36e16fbc564c31c7ffc0ab7ba0e2cf9721a
jih3508 <jih3508@ibizsoftware.net>
2022-06-07 21:19:34 +0900

    01-Activity

--- Android_Basic/안드로이드 기능/01-Activity/app/kotlin/TestActivity.kt
+++ Android_Basic/안드로이드 기능/01-Activity/app/kotlin/TestActivity.kt
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 
-class MainActivity : AppCompatActivity() {
+class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_test)
-        // 한번만 하면 되는 작업
+
         Log.d("LifeCycle", "onCreate")
     }
 
